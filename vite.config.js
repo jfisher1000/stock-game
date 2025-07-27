01b2326205e1ece7e1e071e4a2a967b7fb15d840
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-27 15:21:35 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add resolve.alias to configure absolute import paths.
+  // This allows you to import files using "@/" instead of relative paths.
+  // For example: import MyComponent from '@/components/MyComponent'
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
