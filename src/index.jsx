be9d5f960f38db47dfa2eef8865a91eb0aff185b
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-27 12:08:45 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // This line imports all your Tailwind styles.
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// The 'reportWebVitals' import has been removed as it's not used in Vite.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
