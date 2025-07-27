4aeef54c215aa0b0583447256c3a20c3edca4b20
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-27 13:32:17 -0700

    Update AlertsPage.jsx

--- src/pages/AlertsPage.jsx
+++ src/pages/AlertsPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import PendingInvitations from '../components/competition/PendingInvitations';
 
-const AlertsPage = ({ user }) => {
+const AlertsPage = () => {
     return (
-        <div className="p-8 text-white">
+        <div className="p-8">
             <h1 className="text-4xl font-bold mb-6">Alerts</h1>
-            <PendingInvitations user={user} />
-            {/* You can add other types of alerts here in the future */}
+            <p className="text-muted-foreground">This feature is coming soon.</p>
         </div>
     );
 };
