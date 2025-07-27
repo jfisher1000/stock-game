dddbabb340c89faf15203992daa85f8024302b96
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-27 12:34:28 -0700

    Update Icons.jsx

--- src/components/common/Icons.jsx
+++ src/components/common/Icons.jsx
@@ -22,6 +22,11 @@ export const LogoutIcon = ({ className = "w-6 h-6" }) => (
     <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" /></svg>
 );
 
+// --- Added Calendar Icon ---
+export const CalendarIcon = ({ className = "w-5 h-5" }) => (
+    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+);
+
 
 // --- Navigation Icons ---
 
