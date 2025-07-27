9e948c44f26a6cf212df127e9e5c91dbc8cff2dd
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-26 18:17:42 -0700

    Create firebase.js

---
+++ src/api/firebase.js
@@ -0,0 +1,21 @@
+// src/api/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the instances for use in other files
+export const auth = getAuth(app);
+export const db = getFirestore(app);
