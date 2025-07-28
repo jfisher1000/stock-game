9782bf5859721137e694a0f525d1a7a0230183f7
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-07-27 17:19:13 -0700

    Create toaster.jsx

---
+++ src/components/ui/toaster.jsx
@@ -0,0 +1,45 @@
+// src/components/ui/toaster.jsx
+
+/**
+ * @fileoverview A component that renders toast notifications.
+ *
+ * This file is adapted from shadcn/ui. The Toaster component is responsible
+ * for rendering all toast notifications in the application. It should be
+- * placed at the root of the application so that toasts can be displayed
++ * placed at the root of the application so that toasts can be displayed
+ * from any page or component.
+ */
+
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast";
+import { useToast } from "@/hooks/use-toast";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        );
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
